907c7f125cf1f71b2122d88e8eb085ceadc0fc63
roofimon <roofimon@gmail.com>
2015-12-26 09:27:58 +0700

    Add build on save for vscode

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,19 @@
 class Fizzbuzz{
-    count(value:Number){
-      return "1"
+    count(value:number){       
+        if(this.isModulusByThree(value) && this.isModulusByFive(value))
+            return "fizzbuzz"
+        if(this.isModulusByThree(value))
+            return "fizz"
+         if(this.isModulusByFive(value))
+             return "buzz"
+        return String(value)      
+    }
+        
+    isModulusByFive(value:number): boolean {
+        return value % 5 == 0
+    }
+    
+    isModulusByThree(value:number): boolean {
+        return value % 3 == 0
     }
 }
--- tests/fizzbuzzTest.ts
+++ tests/fizzbuzzTest.ts
@@ -1,6 +1,26 @@
 describe("fizzbuzz", function(){
-  it("should count 1", ()=>{
-      var fizzbuzz = new Fizzbuzz()
+  var fizzbuzz
+  beforeEach(()=>{
+      fizzbuzz = new Fizzbuzz()
+  })
+  
+  it("should count 1", ()=>{      
       expect(fizzbuzz.count(1)).toEqual("1")
+  })
+  
+  it("should count 2", ()=>{
+      expect(fizzbuzz.count(2)).toEqual("2")
+  })
+  
+  it("should count fizz", ()=>{
+      expect(fizzbuzz.count(3)).toEqual("fizz")
+  })
+  
+  it("should count buzz", ()=>{
+      expect(fizzbuzz.count(5)).toEqual("buzz")
+  })
+  
+    it("should count fizzbuzz", ()=>{
+      expect(fizzbuzz.count(15)).toEqual("fizzbuzz")
   })
 })
